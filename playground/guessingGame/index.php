a4c92f6fedf0b823108dbedde11e6bd6da6cf752
Zach <zchrbell7@gmail.com>
2017-09-07 18:32:32 -0600

    asdfasdf

---
+++ playground/guessingGame/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Zac Bell">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Random Number Guessing Game</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="homeDiv">asdfasd</div>
+
+</body>
+</html>
---
+++ playground/guessingGame/model.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zac
+ * Date: 9/7/17
+ * Time: 6:12 PM
+ */
